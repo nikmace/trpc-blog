20dd012e998a46fbad4abf465183a45c4a782094
nikmace <nikmacentosh@gmail.com>
2022-08-21 19:52:10 +0300

    Constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const URL = `${BASE_URL}/api/trpc`;
