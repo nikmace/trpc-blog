fe29ea203538cc9a668d883e0b9c57d04ff9a11e
nikmace <nikmacentosh@gmail.com>
2022-08-21 19:52:48 +0300

    Update a post page

---
+++ src/pages/posts/update/[postId].tsx
@@ -0,0 +1,95 @@
+import Error from "next/error";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { trpc } from "../../../utils/trpc";
+
+export default function UpdatePage() {
+  const router = useRouter();
+
+  const postId = router.query.postId as string;
+
+  const { data, isLoading } = trpc.useQuery(["posts.single-post", { postId }]);
+
+  if (isLoading) {
+    return <p>Loading posts...</p>;
+  }
+
+  if (!data) {
+    return <Error statusCode={404} />;
+  }
+  return (
+    <>
+      <div className="h-[80vh]">
+        <div className="min-h-full  flex content-center items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="mt-5 md:mt-0 max-w-[600px] w-full">
+            <form className="">
+              <div className="shadow sm:rounded-md sm:overflow-hidden">
+                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                  <div className="grid grid-cols-3 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="title"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Title
+                      </label>
+                      <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        autoComplete=""
+                        value={data.title}
+                        className="p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="body"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Body
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        id="body"
+                        name="body"
+                        rows={3}
+                        className="p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        placeholder="Text apppears here.."
+                        defaultValue={""}
+                        value={data.body}
+                      />
+                    </div>
+                    <p className="mt-2 text-sm text-gray-500">
+                      Brief description for your profile. URLs are hyperlinked.
+                    </p>
+                  </div>
+                </div>
+
+                {/** More inputs here */}
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="mr-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Update
+                  </button>
+                  <Link href="/posts">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md border-indigo-600 text-indigo-600 hover:text-white hover:bg-indigo-600 focus:ring-indigo-500"
+                    >
+                      Go back
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
