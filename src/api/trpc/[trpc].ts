c6ac0e7971ae762205c715fc9349a22fbac7448c
nikmace <nikmacentosh@gmail.com>
2022-08-21 11:18:25 +0300

    tRPC api endpoint

---
+++ src/api/trpc/[trpc].ts
@@ -0,0 +1,15 @@
+import * as trpcNext from "@trpc/server/adapters/next";
+import { appRouter } from "../../server/route/app.router";
+import { createContext } from "../../server/createContext";
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError({ error }) {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      console.log("Something went wrong with the server", error);
+    } else {
+      console.log(error);
+    }
+  },
+});
