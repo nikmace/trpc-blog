19ad8fc14ff5ce62400ee16354ea86825c33fd73
nikmace <nikmacentosh@gmail.com>
2022-08-22 18:33:15 +0300

    Getting user from Redux store

--- src/components/SinglePostHeading.tsx
+++ src/components/SinglePostHeading.tsx
@@ -16,6 +16,8 @@ import {
 import { Menu, Transition } from "@headlessui/react";
 import { useUserCtx } from "../context/user.context";
 import Link from "next/link";
+import { useSelector } from "react-redux";
+import { selectAuthUser } from "../redux/auth/selectors";
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
@@ -28,8 +30,13 @@ interface Props {
   id: string;
 }
 
-export default function SinglePostHeading({ title, createdAt, userId, id }: Props) {
-  const user = useUserCtx();
+export default function SinglePostHeading({
+  title,
+  createdAt,
+  userId,
+  id,
+}: Props) {
+  const authUser = useSelector(selectAuthUser);
 
   return (
     <div className="lg:flex lg:items-center lg:justify-between my-16">
@@ -64,20 +71,19 @@ export default function SinglePostHeading({ title, createdAt, userId, id }: Prop
         </div>
       </div>
       <div className="mt-5 flex lg:mt-0 lg:ml-4">
-        {user?.id === userId && (
+        {authUser.id === userId && (
           <span className="hidden sm:block">
             <Link href={`/posts/update/${id}`}>
-            <button
-              type="button"
-              className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              <PencilIcon
-                className="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                aria-hidden="true"
-              />
-              Edit
-            </button>
-
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <PencilIcon
+                  className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                  aria-hidden="true"
+                />
+                Edit
+              </button>
             </Link>
           </span>
         )}
