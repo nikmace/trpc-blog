50c77adcc9fa87ae583f1a968664e44809d36f3c
nikmace <nikmacentosh@gmail.com>
2022-08-24 15:50:56 +0300

    Added schemas

--- src/schema/post.schema.ts
+++ src/schema/post.schema.ts
@@ -1,8 +1,11 @@
 import z from "zod";
 
 export const createPostSchema = z.object({
-  title: z.string().max(256, "Max title length is 256 characters"),
-  body: z.string().min(10),
+  title: z
+    .string()
+    .min(5, "Minimum title length is 5 characters")
+    .max(256, "Maximum title length is 256 characters"),
+  body: z.string().min(10, "Minimum body length is 10 characters"),
 });
 
 export type CreatePostInput = z.TypeOf<typeof createPostSchema>;
@@ -10,3 +13,18 @@ export type CreatePostInput = z.TypeOf<typeof createPostSchema>;
 export const getSinglePostSchema = z.object({
   postId: z.string().uuid(),
 });
+
+export const deletePostSchema = z.object({
+  postId: z.string().uuid(),
+});
+
+export const updatePostSchema = z.object({
+  postId: z.string().uuid(),
+  title: z
+    .string()
+    .min(5, "Minimum title length is 5 characters")
+    .max(256, "Maximum title length is 256 characters"),
+  body: z.string().min(10, "Minimum body length is 10 characters"),
+});
+
+export type UpdatePostInput = CreatePostInput;
