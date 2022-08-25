7f9da9cd5e6b598774e58231ae38ecea4aeb64e6
nikmace <nikmacentosh@gmail.com>
2022-08-25 17:36:10 +0300

    Moved Profile Avatar to separate component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,28 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useEffect, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import Link from "next/link";
 
 import Logo from "../public/images/talkey3.png";
 import { selectAuthenticated } from "../redux/auth/selectors";
-import { logoutUser } from "../redux/auth/slice";
+import ProfileAvatar from "./profile/ProfileAvatar";
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
-  const dispatch = useDispatch();
   const isAuthenticated = useSelector(selectAuthenticated);
   let navigation = useMemo(
     () => [
-      { name: "Dashboard", href: "#", current: true },
       { name: "Team", href: "#", current: false },
       { name: "Posts", href: "/posts", current: false },
     ],
     []
   );
-
-  const handleLogout = () => {
-    dispatch(logoutUser());
-  };
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -74,6 +68,21 @@ export default function Navbar() {
                         </p>
                       </Link>
                     ))}
+                    {isAuthenticated && (
+                      <Link href={"/dashboard"} key={"dashboard"}>
+                        <p
+                          className={classNames(
+                            false
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={false ? "page" : undefined}
+                        >
+                          Dashboard
+                        </p>
+                      </Link>
+                    )}
                     {!isAuthenticated && (
                       <>
                         <Link href={"/login"} key={"login"}>
@@ -107,80 +116,7 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <Image
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                        height={32}
-                        width={32}
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <p
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Your Profile
-                          </p>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <p
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Settings
-                          </p>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <p
-                            onClick={handleLogout}
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                          >
-                            Sign out
-                          </p>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+              <ProfileAvatar classNames={classNames} />
             </div>
           </div>
 
