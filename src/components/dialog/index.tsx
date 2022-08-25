130d7b5e8c82d18582f3dd9e6ab067646ada15b2
nikmace <nikmacentosh@gmail.com>
2022-08-25 17:36:52 +0300

    New dialog component for deleting post

---
+++ src/components/dialog/index.tsx
@@ -0,0 +1,89 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+interface DialogPromptProps {
+  openDialog: boolean;
+  setOpenDialog: (value: boolean) => void;
+  deletePost: () => Promise<void>;
+}
+
+const DialogPrompt: React.FC<DialogPromptProps> = ({
+  openDialog,
+  setOpenDialog,
+  deletePost,
+}) => {
+  const closeDialog = () => {
+    setOpenDialog(false);
+  };
+
+  return (
+    <Transition appear show={openDialog} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeDialog}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Do you wish to delete this post?
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Your action will be irreversible. Cancel if you don&apos;t
+                    want to delete this post.
+                  </p>
+                </div>
+
+                <div className="flex justify-between">
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeDialog}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      onClick={deletePost}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default DialogPrompt;
