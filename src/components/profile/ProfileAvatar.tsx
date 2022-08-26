b8bed0d12577f53004e0b110ac0d4164e6464939
nikmace <nikmacentosh@gmail.com>
2022-08-26 08:43:21 +0300

    Changed styling

--- src/components/profile/ProfileAvatar.tsx
+++ src/components/profile/ProfileAvatar.tsx
@@ -27,14 +27,14 @@ const ProfileAvatar: React.FC<ProfileAvatarProps> = ({ classNames }) => {
     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
       <button
         type="button"
-        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+        className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-indigo-300"
       >
         <span className="sr-only">View notifications</span>
         <BellIcon className="h-6 w-6" aria-hidden="true" />
       </button>
 
       {/* Profile dropdown */}
-      <Menu as="div" className="ml-3 relative">
+      <Menu as="div" className="ml-3 relative z-10">
         <div>
           <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
             <span className="sr-only">Open user menu</span>
--- src/components/singlepost/index.tsx
+++ src/components/singlepost/index.tsx
@@ -9,12 +9,13 @@ interface PostProps {
 
 const SinglePost: React.FC<PostProps> = ({ id, title, body }) => {
   return (
-    <div className="p-6 h-min max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="p-6 h-min max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-indigo-200 dark:border-indigo-300">
       <a href="#">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
       </a>
+      <p className="mb-1 text-gray-500">by Joe</p>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
         {body.slice(0, 180)}..
       </p>
@@ -25,16 +26,18 @@ const SinglePost: React.FC<PostProps> = ({ id, title, body }) => {
         >
           Read more
           <svg
-            aria-hidden="true"
-            className="ml-2 -mr-1 w-4 h-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-[18px] h-[18px]"
           >
             <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
           </svg>
         </a>
       </Link>
