8a7a3d42bb0e5d41fbd78325623052f96b091592
nikmace <nikmacentosh@gmail.com>
2022-08-26 08:42:38 +0300

    Added white bg and removed link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
   );
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-white">
       {({ open }: any) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -42,15 +42,17 @@ export default function Navbar() {
                 </Disclosure.Button>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <Image
-                    className="block lg:hidden h-8 w-auto"
-                    src={Logo}
-                    alt="Workflow"
-                    height={32}
-                    width={40}
-                  />
-                </div>
+                <Link href="/">
+                  <div className="flex-shrink-0 flex items-center">
+                    <Image
+                      className="block lg:hidden h-8 w-auto"
+                      src={Logo}
+                      alt="Workflow"
+                      height={32}
+                      width={40}
+                    />
+                  </div>
+                </Link>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
@@ -59,7 +61,7 @@ export default function Navbar() {
                           className={classNames(
                             item.current
                               ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              : "font-medium text-gray-500 hover:text-gray-900",
                             "px-3 py-2 rounded-md text-sm font-medium"
                           )}
                           aria-current={item.current ? "page" : undefined}
@@ -74,7 +76,7 @@ export default function Navbar() {
                           className={classNames(
                             false
                               ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              : "font-medium text-gray-500 hover:text-gray-900",
                             "px-3 py-2 rounded-md text-sm font-medium"
                           )}
                           aria-current={false ? "page" : undefined}
@@ -90,7 +92,7 @@ export default function Navbar() {
                             className={classNames(
                               false
                                 ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                : "font-medium text-gray-500 hover:text-gray-900",
                               "px-3 py-2 rounded-md text-sm font-medium"
                             )}
                             aria-current={false ? "page" : undefined}
@@ -103,7 +105,7 @@ export default function Navbar() {
                             className={classNames(
                               false
                                 ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                : "font-medium text-gray-500 hover:text-gray-900",
                               "px-3 py-2 rounded-md text-sm font-medium"
                             )}
                             aria-current={false ? "page" : undefined}
@@ -130,7 +132,7 @@ export default function Navbar() {
                   className={classNames(
                     item.current
                       ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      : "font-medium text-gray-500 hover:text-gray-900",
                     "block px-3 py-2 rounded-md text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
